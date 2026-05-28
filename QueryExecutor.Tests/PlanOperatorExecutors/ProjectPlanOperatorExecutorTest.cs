d519d45987c914e26f8b3d4f0711ff8a3beb5e9d
Philipp Honsel <philipphonsel@gmail.com>
2026-05-28 11:53:18 +0200

    test(executor): Add ProjectPlanOperatorExecutor test

---
+++ QueryExecutor.Tests/PlanOperatorExecutors/ProjectPlanOperatorExecutorTest.cs
@@ -0,0 +1,26 @@
+using AlliumSativum.QueryExecutor.PopExecutors;
+using AlliumSativum.Shared.Models.ExecutionPlan.PlanOperators;
+using AlliumSativum.Shared.Models.IntermediateModels.Specifiers;
+using FluentAssertions;
+using NUnit.Framework;
+using QueryExecutor.Tests.Utils.Provider;
+
+namespace QueryExecutor.Tests.PlanOperatorExecutors;
+
+public sealed class ProjectPlanOperatorExecutorTest
+{
+    [Test]
+    public async Task Should_Project_Plan_Operator()
+    {
+        var pop = new ProjectPlanOperator([new AttributeSpecifier("cs", "algorithm", "id")])
+        {
+            Children = [new AlgorithmDataProvider()]
+        };
+
+        var result = await new ProjectPlanOperatorExecutor().ExecuteAsync(pop);
+
+        result.ExecutionData.Data.Should().HaveCount(4);
+        result.ExecutionData.Data.Should().AllSatisfy(e => e.Should().ContainKey("cs->algorithm.id"));
+        result.ExecutionData.Data.Should().AllSatisfy(e => e.Should().NotContainKey("cs->algorithm.name"));
+    }
+}
